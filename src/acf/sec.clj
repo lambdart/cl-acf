107a8addd3e50dbef9bd5d0fb2243199bd66359a
lambdart <lambdart@protonmail.com>
2023-06-05 09:47:55 -0300

    Remove sec name space

--- src/acf/sec.clj
+++
@@ -1,9 +0,0 @@
-(ns acf.sec
-  (:import (java.security SecureRandom)))
-
-(defn random-bytes
-  "Returns a random byte array of the specified size."
-  [size]
-  (let [seed (byte-array size)]
-    (.nextBytes (SecureRandom/getInstance "SHA1PRNG") seed)
-    seed))
